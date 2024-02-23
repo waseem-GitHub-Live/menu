e47eeb2f96e2b067ba8c5b45fe172bbb5a6117b9
waseem-GitHub-Live <waseem.terafort@gmail.com>
2024-02-23 14:58:26 +0500

    gradle

--- app/src/main/java/com/waseem/akram/MainActivity.kt
+++ app/src/main/java/com/waseem/akram/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.waseem.akram.R
 import com.waseem.akram.ui.theme.ZikirmatikTheme
 
 class MainActivity : ComponentActivity() {
